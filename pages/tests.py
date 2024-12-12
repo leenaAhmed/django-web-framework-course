27ac7dea3ccff10f68c44b884d3c9111313aff06
leenaAhmed <leena3792@gmail.com>
2024-12-12 15:03:01 +0200

    feat: add test case to contact us models

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
-
+from datetime import datetime
+from .models import Contact
 # Create your tests here.
+
+
+class UserTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.contact = Contact.objects.create(
+            full_name= "Mohamed Ahmed",
+            email = 'leenaelhosary@gmail.com'
+        )
+    
+    def test_fields(self):
+        self.assertIsInstance(self.contact.full_name, str)
+        self.assertIsInstance(self.contact.email, str)
+
+    def test_timestamp(self):
+        self.assertIsInstance(self.contact.created_at , datetime)
+
+        
